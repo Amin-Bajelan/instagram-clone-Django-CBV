35f658e353cb9bb5ae54731def68c6ac5388d035
Amin-Bajelan <m.amin.bajelan@gmail.com>
2025-07-16 01:23:02 +0330

    create url and view like post

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index_post'),
     path('explore/', views.ExploreView.as_view(), name='explore_post'),
+    path('like/<int:post_id>/', views.LikePostView.as_view(), name='like_post'),
+    path('list-likes/<int:post_id>', views.ListLikesView.as_view(), name='likes_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView,TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, TemplateView, View
 from .models import Post
 from accounts.models import Profile
 
@@ -24,3 +24,30 @@ class ExploreView(ListView):
 
     def get_queryset(self):
         return Post.objects.filter(user__profile__is_private=False).order_by('-date_posted')
+
+
+class LikePostView(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        user = request.user
+
+        if user in post.liked_by.all():
+            post.liked_by.remove(user)
+            post.likes -= 1
+        else:
+            post.liked_by.add(user)
+            post.likes += 1
+
+        post.save()
+        return redirect('/posts/index/')
+
+
+class ListLikesView(ListView):
+    template_name = 'posts/likes_list.html'
+    context_object_name = 'liked_users'
+    model = Post
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+        return post.liked_by.all()
